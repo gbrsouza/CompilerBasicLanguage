065e9c8c928613d24c8c232c06f77d0004a7feec
André Winston Arruda Skeete <a.winston.s97@gmail.com>
2019-08-07 11:45:24 -0300

    Creation of structs to represent tikz lines of code

--- src/astToTikz.cpp
+++ src/astToTikz.cpp
@@ -11,9 +11,7 @@ using std::cin;
 using std::cout;
 using std::string;
 using std::vector;
-using std::deque
-
-using namespace std;
+using std::deque;
 
 struct Node{
 	Node *father;
@@ -23,17 +21,48 @@ struct Node{
 };
 
 struct TikzNodeDef{
+	//								 nodeType			nodeId			 	positioning			  nodeContent
+	// sample node definition: \node[roundnode]        (uppercircle)       [above=of maintopic]  {     1     };
 	string nodeType;
 	string nodeId;
 	string positioning;
 	string nodeContent;
+	TikzNodeDef(string nt, string nid, string p, string nc) : 
+		nodeType(nt), 
+		nodeId(nid), 
+		positioning(p), 
+		nodeContent(nc)
+	{
+	}
+};
+
+struct TikzEdgeDef{
+	//									  idFirst     regionFirst	   idSecond  regionSecond
+	// sample edge definition: \draw[->] (uppercircle.south)   --     (maintopic.north);
+	string idFirst;
+	string regionFirst;
+	string idSecond;
+	string regionSecond;
+	TikzEdgeDef(string i1, string r1, string i2, string r2) : 
+		idFirst(i1), 
+		regionFirst(r1), 
+		idSecond(i2), 
+		regionSecond(r2)
+	{
+	}
+};
+
+string symbol_to_string(symbol s){
+	return "TODO";
 }
 
 string astToTikz(Node *root){
-	string ret = "\\begin{tikzpicture}[\nroundnode/.style={circle, draw=green!60, fill=green!5, very thick, minimum size=7mm},\nsquarednode/.style={rectangle, draw=red!60, fill=red!5, very thick, minimum size=5mm},\n]";
+	string ret = "\\begin{tikzpicture}[\nterminalNode/.style={circle, draw=black!60, fill=black!5, very thick, minimum size=7mm},\nnonterminalNode/.style={rectangle, draw=blue!60, fill=blue!5, very thick, minimum size=5mm},\n]";
 
 	vector<TikzNodeDef> nodeDefs;
 	vector<TikzEdgeDef> edgeDefs;
+
+	nodeDefs.push_back(TikzNodeDef(root->children.empty()? "terminalNode" : "nonterminalNode", "0", "", root->lexeme == "" ? symbol_to_string(root->sym) : root->lexeme));
 
 	deque<Node*> q;
 	q.push_back(root);
@@ -41,8 +70,11 @@ string astToTikz(Node *root){
 	while(!q.empty()){
 		Node *u = q.front();
 		q.pop_back();
-		for(auto v : u->children){
+		TikzNodeDef def = TikzNodeDef(u->children.empty() ? "terminalNode" : "nonterminalNode", std::to_string(nodeDefs.size()), "", "");
+		def.nodeId = nodeDefs.size();
 
+		for(auto v : u->children){
+			
 		}
 	}
 	
