5c3241057296fef74b12a55aa6b62b2d15ba9dd5
Victor Agnez lima <victor.agnez@hotmail.com>
2019-10-09 10:40:19 -0300

    Add visit(def_stmt)

--- src/visitor.cpp
+++ src/visitor.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <string>
 #include <type_traits>
@@ -9,8 +10,6 @@
 
 using namespace ast;
 
-using std::cout;
-using std::endl;
 using std::map;
 using std::pair;
 using std::string;
@@ -29,12 +28,12 @@ string to_string(yytokentype value){
 		{(yytokentype)DEF, "DEF"},
 		{(yytokentype)DIFF, "DIFF"},
 		{(yytokentype)DIM, "DIM"},
-		{(yytokentype)DIVIDE, "DIVIDE"},
+		{(yytokentype)DIVIDE, "/"},
 		{(yytokentype)END, "END"},
 		{(yytokentype)ENDL, "ENDL"},
 		{(yytokentype)EQUALS, "EQUALS"},
 		{(yytokentype)EXP, "EXP"},
-		{(yytokentype)EXPONENTIAL, "EXPONENTIAL"},
+		{(yytokentype)EXPONENTIAL, "^"},
 		{(yytokentype)FLOAT, "FLOAT"},
 		{(yytokentype)FOR, "FOR"},
 		{(yytokentype)FUNCTION, "FUNCTION"},
@@ -53,12 +52,12 @@ string to_string(yytokentype value){
 		{(yytokentype)LPAREN, "LPAREN"},
 		{(yytokentype)LT, "LT"},
 		{(yytokentype)LTE, "LTE"},
-		{(yytokentype)MINUS, "MINUS"},
-		{(yytokentype)MOD, "MOD"},
+		{(yytokentype)MINUS, "-"},
+		{(yytokentype)MOD, "%"},
 		{(yytokentype)NEXT, "NEXT"},
-		{(yytokentype)NOT, "NOT"},
-		{(yytokentype)OR, "OR"},
-		{(yytokentype)PLUS, "PLUS"},
+		{(yytokentype)NOT, "!"},
+		{(yytokentype)OR, "||"},
+		{(yytokentype)PLUS, "+"},
 		{(yytokentype)PRINT, "PRINT"},
 		{(yytokentype)READ, "READ"},
 		{(yytokentype)RETURN, "RETURN"},
@@ -72,7 +71,7 @@ string to_string(yytokentype value){
 		{(yytokentype)STRING, "STRING"},
 		{(yytokentype)TAN, "TAN"},
 		{(yytokentype)THEN, "THEN"},
-		{(yytokentype)TIMES, "TIMES"},
+		{(yytokentype)TIMES, "*"},
 		{(yytokentype)TO, "TO"},
 		{(yytokentype)VARIABLE, "VARIABLE"},
 		{(yytokentype)WHITE, "WHITE"},
@@ -93,7 +92,8 @@ inline string to_string(string* value){
 }
 
 vector<string> labels;
-map<int, pair<string, string>> calls;
+map<int, pair<string, string>> calls; // (idx, (parameter_expr, function_name))
+map<string, pair<string, expr*> > function_code; // (label, (parameter_name, code))
 string buffer;
 
 void visitor::visit(const position& node) const{ }
@@ -101,20 +101,61 @@ void visitor::visit(const position& node) const{ }
 void visitor::visit(const token& node) const{ }
 
 void visitor::visit(const program& node) const{
+	string code, main;
+	
 	for(auto _stmt = node.stmts.cbegin(); _stmt != node.stmts.cend(); _stmt++){
 		(*_stmt)->accept(*this);
+		main += buffer;
 	}
+	
+	std::ifstream in("src/activation_records.cpp");
+	
+	using std::cin;
+	
+	std::streambuf *cinbuf = cin.rdbuf();
+	cin.rdbuf(in.rdbuf());
+	
+	string line;
+	do{
+		std::getline(cin, line);
+		code += line + "\n";
+	} while(line.find("enum label") == string::npos);
+
+	for(string label : labels){
+		code += label + ",\n";
+	}
+	
+	do{
+		std::getline(cin, line);
+		code += line + "\n";
+	} while(line.find("int main") == string::npos);
+	
+	code += main;
+	
+	while(std::getline(std::cin, line)){
+		code += line + "\n";
+	}
+	
+	cin.rdbuf(cinbuf);
+	
+	buffer = code;
+	std::cout << buffer;
 }
 
-void visitor::visit(const end_stmt& node) const{ }
+void visitor::visit(const end_stmt& node) const{
+	string label = "l" + to_string(node.line);
+	labels.push_back(label);
+	string code = label + ":\n";
+	code += "return 0;\n";
+	buffer = code;
+}
 
 void visitor::visit(const let_stmt& node) const{
-	int line = node.line;
-	string label = "l" + to_string(line);
+	string label = "l" + to_string(node.line);
 	labels.push_back(label);
 	const auto& var = node.var;
 	const auto& val = node.val;
-	string code = "\t" + label + ":\n";
+	string code = label + ":\n";
 	if(var->idx1 != nullptr){		
 		solve_expr(this, *var->idx1, label + "_idx1", "idx1");
 		code += buffer;
@@ -135,8 +176,8 @@ void visitor::visit(const let_stmt& node) const{
 	}
 	solve_expr(this, *val, label + "_val", "target");
 	code += buffer;
+	code += "let(\"" + *var->name + "\", idx1, idx2, target);\n";
 	buffer = code;
-	cout << "buffer:\n" << buffer << endl;
 }
 
 void visitor::visit(const print_stmt& node) const{ }
@@ -155,7 +196,16 @@ void visitor::visit(const gosub_stmt& node) const{ }
 
 void visitor::visit(const return_stmt& node) const{ }
 
-void visitor::visit(const def_stmt& node) const{ }
+void visitor::visit(const def_stmt& node) const{
+	string label = "l" + to_string(node.line);
+	labels.push_back(label);
+	string label_impl = *node.name + "_" + to_string(node.line);
+	labels.push_back(label_impl);
+	function_code[label_impl] = std::make_pair(*node.param, node.target);
+	string code = label + ":\n";
+	code += "update_def(\"" + *node.name + "\", " + label_impl + ");\n";
+	buffer = code;
+}
 
 void visitor::visit(const dim_stmt& node) const{ }
 
@@ -167,8 +217,10 @@ void visitor::visit(const stop_stmt& node) const{ }
 
 void visitor::visit(const binary_expr& node) const{
 	node.left->accept(*this);
-	buffer += " " + to_string(node.tok.id) + " ";
+	string code = buffer;
+	code += " " + to_string(node.tok.id) + " ";
 	node.right->accept(*this);
+	buffer = code + buffer;
 }
 
 void visitor::visit(const unary_expr& node) const{ }
@@ -194,7 +246,7 @@ void visitor::visit(const function_expr& node) const{
 void visitor::visit(const variable& node) const{ }
 
 template<class T> void visitor::visit(const literal_expr<T>& node) const{
-	buffer += to_string(node.value);
+	buffer = to_string(node.value);
 }
 
 template void visitor::visit<string*>(const literal_expr<string*>& node) const;
@@ -205,14 +257,22 @@ void solve_expr(const visitor* vis, const expr& exp, string label, string target
 	string code = "value " + target + ";\n";
 	code += "{\n";
 	exp.accept(*vis);
-	for(auto t : calls){
+	for(auto it = calls.cbegin(); it != calls.cend(); it++){
+		for(auto it2 = calls.cbegin(); it2 != it; it2++){
+			code += "push_value(t" + to_string(it2->first) + ");\n";
+		}
+		auto& t = *it;
 		code += "next_label = get_def(\"" + t.second.second + "\");\n";
 		string new_label = label + "_t" + to_string(t.first);
+		labels.push_back(new_label);
 		code += "push_function_call(" + new_label + ");\n";
 		code += "push_parameter(" + t.second.first + ");\n";
-		code += "goto transfer;";
+		code += "goto transfer;\n";
 		code += new_label + ":\n";		
 		code += "value t" + to_string(t.first) + " = " + "get_return_value();\n";
+		for(auto it2 = calls.cbegin(); it2 != it; it2++){
+			code += "t" + to_string(it2->first) + " = pop_value();\n";
+		}
 	}
 	calls.clear();
 	code += target + " = " + buffer + ";\n";
@@ -221,11 +281,11 @@ void solve_expr(const visitor* vis, const expr& exp, string label, string target
 }
 
 string verify_index(string idx){
-	string code = "verify_index("+idx+");\n";
+	string code = "verify_index(" + idx + ");\n";
 	return code;
 }
 
 string create_default_index(string idx){
-	string code = "value "+ idx + " = -1;\n";
-	return code;
+	return "int " + idx + " = -1;\n";
 }
+
